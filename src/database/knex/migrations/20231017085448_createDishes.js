98f1f8fd52a10a306827503300f3f0f2b08eaaed
alison <alisonsb.ac@outlook.com>
2023-11-04 10:39:32 -0300

    fix error at create dishes table

--- src/database/knex/migrations/20231017085448_createDishes.js
+++ src/database/knex/migrations/20231017085448_createDishes.js
@@ -9,7 +9,7 @@ exports.up = knex => knex.schema.createTable("dishes" , table => {
     table.integer("user_id").references("id").inTable("users")
 
     table.timestamp("created_at").default(knex.fn.now())
-    table.timestamp("updated").default(knex.fn.now())
+    table.timestamp("updated_at").default(knex.fn.now())
 
 })
 
